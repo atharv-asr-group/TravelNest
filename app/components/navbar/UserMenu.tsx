776adc4b9a4ea41b2c6f4b6ea14303848a8dfcd6
unknown <atharv21240@iiit.ac.in>
2023-07-01 09:54:11 +0530

    trips reservation screeen made  VII I,0

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -8,6 +8,7 @@ import useLoginModal from '@/app/hooks/useLoginModal';
 import { signOut } from 'next-auth/react';
 import { SafeUser } from '@/app/types';
 import useRentModal from '@/app/hooks/useRentModal';
+import { useRouter } from 'next/navigation';
 
 interface UserMenuProps{
     currentUser?: SafeUser |null
@@ -15,6 +16,7 @@ interface UserMenuProps{
 const UserMenu: React.FC<UserMenuProps> = ({
     currentUser
 }) => {
+    const router = useRouter();
     const registerModal=useRegisterModal();
     const loginModal = useLoginModal();
     const rentModal = useRentModal();
@@ -90,7 +92,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                      {currentUser?(
                      <>
                      <MenuItem
-                     onClick={()=>{}}
+                     onClick={()=>router.push('/trips')}
                      label='My Trips'/>
                      <MenuItem
                      onClick={()=>{}}
