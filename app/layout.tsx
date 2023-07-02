50bbe4c03310e4653292b8b4a711944a764a1cf4
unknown <atharv21240@iiit.ac.in>
2023-07-03 01:16:02 +0530

    fixed words

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ import ClientOnly from './components/ClientOnly';
 import getCurrentUser from './actions/getCurrentUser';
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb Clone',
+  title: 'TravelNest',
+  description: 'TravelNest is a booking website',
 }
 
 const font = Nunito({ 
